2159c3310f482a2ea6a3140712ea4d98c1b77757
sam <sam@sphericalcommunications.com>
2014-12-03 15:13:44 -0500

    Updating header
    Dino - this is from the live site

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -41,6 +41,8 @@
 	
 	<!-- favicon -->
 
+	<!-- MILO -->
+
 	<link rel="shortcut icon" href="<?php bloginfo ('template_url'); ?>/icfavicon.png" type="image/png">
 	<link rel="icon" href="icfavicon.png" type="image/png">
 	
@@ -119,7 +121,7 @@
 	?>
 
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 
 var _gaq = _gaq || [];
 
@@ -142,21 +144,31 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 
 })();
 
+</script>-->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48295809-1', 'auto');
+  ga('send', 'pageview');
+
 </script>
 
 <!-- zdirect script -->
 <script type="text/javascript" src="https://www.zdirect.com/scripts/newApp.js"></script>
 
-<!-- Sojern -->
+<!-- sojern script -->
 <script>
 (function () {
 var pl = document.createElement('script');
 pl.type = 'text/javascript';
 pl.async = true;
-pl.src = 'https://beacon.sojern.com/pixel/p/3031';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
+pl.src = 'https://beacon.sojern.com/pixel/p/3679?cid=[destination_searched]&ha1=[destination_searched]&et=hs';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
 })();
 </script>
-<!-- End Sojern -->
 
 </head> 
 	
