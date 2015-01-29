4df2764ad96aae6064986e5a019a92fb7dd8196c
Mandino <dino@misfit-inc.com>
2015-01-29 19:17:46 +0800

    apply universal analytics parameters

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -61,32 +61,33 @@
 	<script type="text/javascript">
 		
 		
-		function createURL() {
-	var checkin = jQuery("#arrival_date").val();
-	var checkout = jQuery("#departure_date").val();
-	var adults = jQuery("#adults").val();
-	var children = jQuery("#children").val();
+	// 	function createURL() {
+	// var checkin = jQuery("#arrival_date").val();
+	// var checkout = jQuery("#departure_date").val();
+	// var adults = jQuery("#adults").val();
+	// var children = jQuery("#children").val();
 	
-	var bookinglink = "<?php echo get_option('cebo_genbooklink'); ?>/search?" + 
-										"&arrival_date=" + checkin + 
-										"&departure_date=" + checkout + 
-										"&adults[]=" + adults + 
-										"&children[]=" + children;
+	// var bookinglink = "<?php echo get_option('cebo_genbooklink'); ?>/search?" + 
+	// 									"&arrival_date=" + checkin + 
+	// 									"&departure_date=" + checkout + 
+	// 									"&adults[]=" + adults + 
+	// 									"&children[]=" + children;
 
-	return bookinglink;
+	// return bookinglink;
 	
-	}
+	// }
 	
-	$(document).ready(function() {
+	// $(document).ready(function() {
 	
 	
-		jQuery('form a.button').click(function(e) {
-					e.preventDefault();
-					_gaq.push(['_link', createURL() ]);
-					return false;
-				});
+	// 	jQuery('form a.button').click(function(e) {
+	// 				e.preventDefault();
+	// 				// _gaq.push(['_link', createURL() ]);
+	// 				createURL();
+	// 				return false;
+	// 			});
 	
-	});
+	// });
 	
 	
 	
@@ -123,34 +124,39 @@
 
 <script type="text/javascript">
 
-var _gaq = _gaq || [];
+// var _gaq = _gaq || [];
 
-_gaq.push(['_setAccount', 'UA-48295809-1']);
-_gaq.push(['_setAllowLinker', true]);
-_gaq.push(['_setDomainName', 'hotelmilosantabarbara.com']);
-_gaq.push(['_trackPageview']);
+// _gaq.push(['_setAccount', 'UA-48295809-1']);
+// _gaq.push(['_setAllowLinker', true]);
+// _gaq.push(['_setDomainName', 'hotelmilosantabarbara.com']);
+// _gaq.push(['_trackPageview']);
 
 
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+// (function() {
+// var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+// ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+// var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+// })();
 
 </script>
 
-<!--
+
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-48295809-1', 'auto');
-  ga('send', 'pageview');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-48295809-1', 'auto', {
+		'allowLinker': true
+	});
+	ga('send', 'pageview');
+	ga('require', 'linker');
+	ga('linker:autoLink', ['reztrip.com'], false, true);
 
 </script>
--->
+
 
 
 <!-- zdirect script -->
@@ -182,7 +188,7 @@ pl.src = 'https://beacon.sojern.com/pixel/p/3679?cid=[destination_searched]&ha1=
 					<div class="reservationform">
 					
 					
-					<form method="get" action="<?php echo get_option('cebo_genbooklink'); ?>/search?" target="_blank">
+					<form method="get" action="<?php echo get_option('cebo_genbooklink'); ?>/search?" target="_self">
 						
 						<span class="calsec">
 							<input type="text"  id="arrival_date" name="arrival_date" placeholder="<?php _e('Arrival','cebolang'); ?>" class="calendarsection" />
@@ -213,8 +219,10 @@ pl.src = 'https://beacon.sojern.com/pixel/p/3679?cid=[destination_searched]&ha1=
 								<option value="3"><?php _e('3 Kids','cebolang'); ?></option>
 							</select>
 						</span>
+
+						<button class="button" type="submit"><?php _e('Search Now','cebolang'); ?></button>
 						
-						<a href="#" class="button" onclick="_gaq.push(['_trackEvent', 'Booking-widget', 'Search-now', 'Search dates with booking widget']);">Search Now</a>	
+						<!-- <a href="#" class="button" onclick="_gaq.push(['_trackEvent', 'Booking-widget', 'Search-now', 'Search dates with booking widget']);">Search Now</a>	 -->
 						
 					
 					</form>
