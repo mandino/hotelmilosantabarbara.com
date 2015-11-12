541bf99a929aaaa9dab71733c8bd911046bdd3df
jomel22 <jomel_bola@yahoo.com>
2015-11-12 23:54:06 +0800

    Revert Colorbox

--- wp-content/themes/ic/library/super-map.php
+++ wp-content/themes/ic/library/super-map.php
@@ -540,9 +540,8 @@
 </script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.simplemodal.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/basic.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/js/jquery.colorbox.js"></script>
 <!--
-
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.colorbox.js"></script>
 	<script>
 		$(document).ready(function(){
 			//Examples of how to assign the ColorBox event to elements
