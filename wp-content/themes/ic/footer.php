e3d51a62478dbc18ecb39d65e83935b8b4c8e86b
Jhelline <jhelline.fausto@outsourced.ph>
2016-05-12 02:37:04 +0800

    fix conflict

--- wp-content/themes/ic/footer.php
+++ wp-content/themes/ic/footer.php
@@ -172,7 +172,6 @@ jQuery( document ).ready(function( $ ) {
 });
 </script>
 
-<<<<<<< HEAD
  <script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new
 
 Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
@@ -180,9 +179,6 @@ Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.p
 ga('create', 'UA-48295809-1', 'auto',{'allowLinker': true });
 
 ga('require', 'linker'); ga('linker:autoLink', ['hotelmilosantabarbara.reztrip.com','hotelmilosantabarbara.reztripmobile.com']);
-=======
-
->>>>>>> h1-tags-jhelline
 
 ga('send', 'pageview');
 
