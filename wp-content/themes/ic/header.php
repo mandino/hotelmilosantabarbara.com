731c1c629268845d6c3523be325e5dfad457784c
Jhelline <jhelline.fausto@outsourced.ph>
2016-05-19 22:03:39 +0800

    open reztrip new tab

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -115,7 +115,7 @@ ga('send', 'pageview');
 				<div class="whippapeal">
 				<div class="formfields">
 					<div class="reservationform">
-						<form method="get" action="<?php echo get_option('cebo_genbooklink'); ?>/search?" target="_self">
+						<form method="get" action="<?php echo get_option('cebo_genbooklink'); ?>/search?" target="_blank">
 							<input type="hidden" value="1" name="rooms">
 							<span class="calsec">
 								<input type="text"  id="arrival_date" name="arrival_date" placeholder="<?php _e('Arrival','cebolang'); ?>" class="calendarsection" />
