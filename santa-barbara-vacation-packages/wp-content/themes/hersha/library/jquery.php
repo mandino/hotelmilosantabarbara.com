56bdbc01ec4fda4f4e118d909845e70b92ba48f2
mandino <dino@misfit-inc.com>
2017-03-21 13:17:41 +0800

    convert to protocol relative urls

--- santa-barbara-vacation-packages/wp-content/themes/hersha/library/jquery.php
+++ santa-barbara-vacation-packages/wp-content/themes/hersha/library/jquery.php
@@ -18,9 +18,9 @@
 <!-- <link rel="stylesheet" type="text/css" href="<?php bloginfo ('template_url'); ?>/css/oceana-hotel.css"> -->
 
 <!-- JS -->
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-<!--<script src="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.js"></script>-->
-<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js?ver=3.5.2'></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.8.2.min.js"></script>
+<!--<script src="//code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.js"></script>-->
+<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js?ver=3.5.2'></script>
 <script type="text/javascript" src="<?php bloginfo ('template_url'); ?>/js/jquery.prettyPhoto.js"></script>
 
 <!-- ClickNav -->
@@ -30,7 +30,7 @@
 <link rel="stylesheet" href="<?php bloginfo ('template_url'); ?>/css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet"/>	
 
 <!-- Pinterest -->
-<script type="text/javascript" src="http://assets.pinterest.com/js/pinit.js"></script>
+<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>
 
 <!-- Custom Modernizr for Quotes Rotator -->
 <script src="<?php bloginfo ('template_url'); ?>/js/quotes-rotator/modernizr.custom.js"></script>
@@ -48,7 +48,7 @@
 <script type="text/javascript" src="<?php bloginfo ('template_url'); ?>/js/jquery.sticky.js"></script>
 
 <!-- Jquery UI -->
-<!-- <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js?ver=3.5.2'></script> -->
+<!-- <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js?ver=3.5.2'></script> -->
 
 <!-- Iosslider -->
 <?php if ( 'rooms' == get_post_type() ) 	{ ?>
