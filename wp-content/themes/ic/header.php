8975c15af235653102610ea889b4fe35c977045c
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2017-04-24 18:29:45 +0800

    ic local link edit

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -167,7 +167,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				</div>
 
 				<ul class="container no-js">
-					<li><a href="//www.independentcollection.com/ic-local" target="_blank" class="clicknav-clicker">Join IC Local and Start Receiving Perks with Every Stay</a></li>
+					<li><a href="//www.iclocalrewards.com" target="_blank" class="clicknav-clicker">Join IC Local and Start Receiving Perks with Every Stay</a></li>
 					<li class="blue-btn"><a href="<?php bloginfo('url'); ?>/blue/"><i class="fa fa-info-circle"></i><span class="blue-mobile">why blue?</span></a></li>
 				</ul>
 			</nav>
